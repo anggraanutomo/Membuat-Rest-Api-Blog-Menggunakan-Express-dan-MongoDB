e9ab0340d453f5741c77f7ff5f3dacf5d20ac532
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-23 21:33:46 +0700

    menambahkan routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+
+const mongoUri = 'mongodb://localhost:27017/bdd-27';
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
