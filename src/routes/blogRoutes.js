a8aaa740a84df6839dc9ed1ab212b37293d2f7fc
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-26 11:42:28 +0700

    add crud rest

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -4,13 +4,43 @@ const Blogpost = mongoose.model('Blogpost');
 
 const router = express.Router();
 
-router.get('/blogposts', (req, res) => {
-  res.send('All Blogposts');
+router.post('/blogposts', async (req, res) => {
+    try {
+        const {title, content} = req.body;
+        const blogpost = new Blogpost({title, content});
+        await blogpost.save();
+        res.send("create a post");
+    } catch (err) {
+        res.status(422).send(err.message); // user send invalid data
+    }
 });
 
-router.post('/blogposts', (req, res) => {
-  console.log(req.body);
-  res.send("create a post");
+router.get('/blogposts', async (req, res) => {
+    const blogposts = await Blogpost.find();
+    res.send(blogposts);
+});
+
+router.get('/blogposts/:id', async (req, res) => {
+    const blogpost = await Blogpost.findOne({_id: req.params.id});
+    res.send(blogpost);
+});
+
+router.put('/blogposts/:id', async (req, res) => {
+    try {
+        await Blogpost.findByIdAndUpdate({_id: req.params.id}, req.body);
+        res.send(await Blogpost.findOne({_id: req.params.id}));
+    } catch ( err) {
+        res.status(422).send(err.message);
+    }
+});
+
+router.delete('/blogposts/:id', async (req, res) => {
+    try {
+        await Blogpost.findByIdAndRemove({_id: req.params.id});
+        res.send("delete a post");
+    } catch (err) {
+        res.status(422).send(err.message);
+    }
 });
 
 module.exports = router;
